6ea9d42d7876f42440cf85896523bc2e0bd597b8
Anthony Rawlins <electrumdatasolutions@gmail.com>
2021-10-21 00:44:56 -0700

    added tooltip regex

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const xmlCheckbox = document.getElementById("newline");
 const results = document.getElementById("results");
 
 const re = /(?<=erml:)\w*\s|[\s\w]*(?=<)/g;
+const re2 = /(?<=tooltip=")[\w\s]*|[\s\w]*(?=<)/g;
 let textList = [];
 
 function getElementValues() {
@@ -19,32 +20,22 @@ function getElementValues() {
     }
     textarea.value = "";
     for (let i = 0; i < textList.length; i++) {
+      let regex = null;
+      textList[i].match("tooltip") ? (regex = re2) : (regex = re);
       // only grab elements with a property and a value
-      if (textList[i].match(re).length > 2) {
-        let xmlProperty = textList[i].match(re)[index1];
-        let xmlVlaue = textList[i].match(re)[index2];
+      if (textList[i].match(regex).length > 2) {
+        let xmlProperty = textList[i].match(regex)[index1];
+        let xmlVlaue = textList[i].match(regex)[index2];
         let xmlData = `${xmlProperty}: ${xmlVlaue}`;
         textarea.value += xmlData + "\n";
       }
     }
   };
 
-  // if xml has line breaks
+  // xml has line breaks
   if (xmlCheckbox.checked) {
-    // const textSplit = text.split("\n");
-    // for (let i = 0; i < textSplit.length; i++) {
-    //   if (textSplit[i].match("erml")) {
-    //     textList.push(textSplit[i]);
-    //   }
-    // }
     loopArray(1, 2, "\n");
   } else {
-    // const textSplit = text.split("><");
-    // for (let i = 0; i < textSplit.length; i++) {
-    //   if (textSplit[i].match("erml")) {
-    //     textList.push(textSplit[i]);
-    //   }
-    // }
     loopArray(0, 1, "><");
   }
 }
