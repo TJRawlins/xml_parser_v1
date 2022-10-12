817c7d951fb07a98b70f95e3c19b1e57ea528866
Anthony Rawlins <electrumdatasolutions@gmail.com>
2022-10-11 23:06:33 -0700

    js update to automate ads removal

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 "use strict";
 const removeAdsBtn = document.getElementById("noAds");
-const search = document.getElementById('.gsc-search-button')
-const pageNum = document.querySelector('.gsc-cursor-page')
 
 function removeAds() {
         if(document.body.contains(document.querySelector('iframe'))) {
                 document.querySelector('iframe').remove()
+                console.log('ads removed');
+                
         } else {
-                alert('Error')
+                // alert('Initiate a search first')
         }
 }
 
 removeAdsBtn.addEventListener('click', ()=> {
         removeAds();
 })
+     
+document.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+                removeAds();
+        }
+});
 
-search.addEventListener('click', ()=> {
+document.onclick= function(event) {
+        if (event===undefined) event= window.event;
+        let target= 'target' in event? event.target : event.srcElement;
         removeAds()
-})
+};
+
+window.onload = removeAds;
+    
+window.onload = function() {
+        let reloading = sessionStorage.getItem("reloading");
+        if (reloading) {
+            sessionStorage.removeItem("reloading");
+            setTimeout(removeAds(),2000)
+            console.log('ads removed');
+            
+        }
+    }
+    
+    function reloadP() {
+        sessionStorage.setItem("reloading", "true");
+        document.location.reload();
+    }
